3b22101c8639b8c69df496dd58922066a51341fd
Devin Shoemaker <devinshoe@gmail.com>
2020-01-26 16:51:55 -0600

    Configure Webpack for Ionic

---
+++ apps/water-watch/webpack.config.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const getWebpackConfig = require('@nrwl/react/plugins/webpack');
+
+function getCustomWebpackConfig(webpackConfig) {
+  const config = getWebpackConfig(webpackConfig);
+
+  // SVG fix
+  delete config.module.rules.find(rule => rule.test.test('.svg')).issuer;
+
+  // // TS v3.7 support
+  // const babelRuleOptions = config.module.rules.find(r => r.loader === 'babel-loader').options;
+  // babelRuleOptions.plugins.push('@babel/plugin-proposal-optional-chaining');
+  // babelRuleOptions.plugins.push('@babel/plugin-proposal-nullish-coalescing-operator');
+
+  // Polyfill Node
+  config.node = {
+    global: true,
+    process: true
+  };
+
+  return config;
+}
+
+module.exports = getCustomWebpackConfig;
