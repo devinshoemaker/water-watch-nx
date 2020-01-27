3479d6ee59006accaaaa37fe9c8c17e0416afc25
Devin Shoemaker <devinshoe@gmail.com>
2020-01-27 12:45:43 -0600

    libs/auth: configure Jest for Ionic

--- libs/auth/jest.config.js
+++ libs/auth/jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/auth'
+  coverageDirectory: '../../coverage/libs/auth',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/lib/__mocks__/fileMock.js'
+  },
+  modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__']
 };
