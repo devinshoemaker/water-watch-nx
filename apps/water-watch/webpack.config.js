fd9f79dc4f820637d2c0df3060777ac29e8a6b94
Devin Shoemaker <devinshoe@gmail.com>
2020-01-27 21:34:21 -0600

    apps/water-watch: remove SVG fix from Webpack config

--- apps/water-watch/webpack.config.js
+++ apps/water-watch/webpack.config.js
@@ -5,7 +5,7 @@ function getCustomWebpackConfig(webpackConfig) {
   const config = getWebpackConfig(webpackConfig);
 
   // SVG fix
-  delete config.module.rules.find(rule => rule.test.test('.svg')).issuer;
+  // delete config.module.rules.find(rule => rule.test.test('.svg')).issuer;
 
   // // TS v3.7 support
   // const babelRuleOptions = config.module.rules.find(r => r.loader === 'babel-loader').options;
